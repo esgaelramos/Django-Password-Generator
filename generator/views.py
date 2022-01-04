939aa274747cabb5ef05e6ae0b9ce24737960ff6
esgaelramos <esgaelramos@gmail.com>
2022-01-04 17:59:29 -0600

    Import libs in views.py and create view about

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+import random
+import generator
 
 # Create your views here.
+def about(request):
+    # return HttpResponse('Hello World Django')
+    return render(request, 'generator/about.html')

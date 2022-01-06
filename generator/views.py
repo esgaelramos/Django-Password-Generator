bede4609df97d45693e22670280d84a76c72e1c5
esgaelramos <esgaelramos@gmail.com>
2022-01-05 18:01:04 -0600

    Create function password like view.request

--- generator/views.py
+++ generator/views.py
@@ -11,3 +11,23 @@ def about(request):
 def home(request):
     return render(request, 'generator/home.html')
 
+def password(request):
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    generated_password = ''
+
+    length = int(request.GET.get('length'))
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('special'):
+        characters.extend(list('@!?-_#$%&/()'))
+    
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+    
+    for x in range(length):
+        generated_password += random.choice(characters)
+
+
+    return render(request, 'generator/password.html', {'password': generated_password})

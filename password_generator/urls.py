3e7a1cdd91cd3e61bdb1f1670cb7a933af815268
esgaelramos <esgaelramos@gmail.com>
2022-01-12 18:11:17 -0600

    Manage urls and add his self.views

--- password_generator/urls.py
+++ password_generator/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import views
 from django.contrib import admin
 from django.urls import path
+from generator import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('about', views.about),
+    path('', views.home),
+    path('generate-password', views.password, name='password'),
+    path('lahipotenusa', views.hipotenusa, name='hipotenusa'),
+    path('resultado', views.tuhipotenusa, name='turesultado'),
 ]
